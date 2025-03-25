34520ab17ac322b28588d72f9ee7dc5bd4293cf1
Kostia Lysak <lisakkosta37@gmail.com>
2025-03-25 21:31:11 +0200

    lab 3

---
+++ lr proga3/lr proga3/lr proga3.cpp
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main() {
+    srand(time(NULL));
+    int n;
+    printf("Enter amount of numbers n: ");
+    scanf_s("%d", &n);
+    if (n < 3) {
+        printf(" n must be bigger than 3.\n");
+        return 0;
+    }
+    else {
+        double prev, current, next;
+        prev = rand() % (101)-50;
+        current = rand() % (101)-50;
+        int i = 2;
+        while (i < n) {
+            next = rand() % (101)-50;
+            if (prev < current && current < next) {
+                
+                printf("Natural i that, %lf < %lf < %lf: %d\n",prev ,current ,next ,i );
+            }
+            prev = current;
+            current = next;
+            i++;
+        }
+    }
+return 0;
+}
