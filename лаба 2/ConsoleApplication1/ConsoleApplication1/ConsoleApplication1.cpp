4856579e05395d0ffc300ce749f6be3c9b9d9d02
Kostia Lysak <lisakkosta37@gmail.com>
2025-03-25 16:38:44 +0200

    Add files via upload

---
+++ лаба 2/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+int Eratos() {
+    int k = 0;
+    const int n = 999;
+    bool arr[n + 1];
+    for (int i = 0; i <= n; i++) {
+        arr[i] = true;
+    }
+    for (int p = 2; p * p <= n; p++) {
+        if (arr[p]) {
+            for (int i = p* p; i <= n; i += p) {
+                arr[i] = false;
+            }
+        }
+    }
+    for (int p = 100; p <= n; p++) {
+        if (arr[p]) {
+            k++;
+           // cout << p << " ";
+        }
+    }
+   // cout << endl<<"amount of odd numbers is: "<< k << endl;
+    return k;
+}
+int main() {
+    clock_t startTime = clock();
+    int k;
+    
+    for (int t = 0; t <= 10000; t++) {
+        k=Eratos();
+    }
+    clock_t endTime = clock();
+    double seconds = (double(endTime - startTime)) / CLOCKS_PER_SEC;
+    cout << k<<endl;
+    cout << "Time of second algorithm is: " << seconds;  
+    return 0;
+}
---
+++ лаба 2/LB2.22/LB2.22/LB2.22.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+int rec(int a, int b) {
+	if (a == b){
+		return b;}
+	else {
+		if (a <= b) {
+			cout << a<<" ";
+			return rec(a+1, b);
+			
+		}
+		else{
+			cout << a<<" ";
+			return rec(a-1,b);
+		
+		}
+	}
+}
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	cout << rec(a, b) << endl<<endl;
+	clock_t startTime = clock();
+	
+	clock_t endTime = clock();
+	double seconds = (double(endTime - startTime)) / CLOCKS_PER_SEC;
+	cout <<endl<< "Time of fourth algorithm is: " << seconds;
+	return 0;
+}
